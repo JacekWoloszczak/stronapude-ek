64af77fa82040f70257565a19af834e1292cd6b5
JacekWoloszczak <jacek.woloszczak@gmail.com>
2024-07-23 13:03:46 +0200

    Dots

--- js/slaider.js
+++ js/slaider.js
@@ -1,7 +1,18 @@
-const leftBtn = document.querySelector(".slaider_btn_left");
-const rightBtn = document.querySelector(".slaider_btn_right");
+const slaiderDots = document.querySelector(".slaider_dots");
+const dots = document.querySelectorAll(".dots");
+const dotFirst = document.querySelector(".dot_first");
+const dotSecond = document.querySelector(".dot_second");
+const dotThird = document.querySelector(".dot_third");
+const dotFourth = document.querySelector(".dot_");
+const dotFifth = document.querySelector(".dot_first");
+
 const slaiderBox = document.querySelector(".slaider_opinion");
 const slaiderContent = document.querySelectorAll(".slaider_content");
+const slaiderContentImgFirst = document.querySelector(".slaider_img-tekst-1");
+const slaiderContentImgSecond = document.querySelector(".slaider_img-tekst-2");
+const slaiderContentImgThird = document.querySelector(".slaider_img-tekst-3");
+const slaiderContentImgFourth = document.querySelector(".slaider_img-tekst-4");
+const slaiderContentImgFifth = document.querySelector(".slaider_img-tekst-5");
 const slaiderWidth = 100;
 const slaiderSpeed = 5000;
 
@@ -9,8 +20,10 @@ let index = 0;
 
 const handleSlaider = () => {
   index++;
+
   changeContent();
 };
+
 let startSlaider = setInterval(handleSlaider, slaiderSpeed);
 const changeContent = () => {
   if (index >= 5) {
@@ -19,19 +32,15 @@ const changeContent = () => {
   if (index < 0) {
     index = 0;
   }
+  if (index === 1) {
+    slaiderContentImgFirst.style.backgroundColor = "red";
+  }
+
+  if (index === 2) {
+    slaiderContentImgSecond.style.backgroundColor = "red";
+  }
 
   slaiderBox.style.transform = `translateX(${-index * slaiderWidth}%)`;
-};
-
-const handleRightButton = () => {
-  index++;
-
-  resetInterval();
-};
-const handleLeftButton = () => {
-  index--;
-
-  resetInterval();
 };
 
 const resetInterval = () => {
@@ -39,5 +48,5 @@ const resetInterval = () => {
   clearInterval(startSlaider);
   startSlaider = setInterval(handleSlaider, slaiderSpeed);
 };
-rightBtn.addEventListener("click", handleRightButton);
-leftBtn.addEventListener("click", handleLeftButton);
+
+dotFirst.addEventListener("click", () => {});
